015b740334f5da9af8c788a7cc5218c8c68012bb
hanhnguyengiahan <hanhnguyengiahan@github.com>
2024-10-12 07:46:49 +1100

    init back-end

---
+++ back-end/src/server.ts
@@ -0,0 +1,75 @@
+import express, { json, Request, Response } from 'express';
+import { echo } from './newecho';
+import morgan from 'morgan';
+import config from './config.json';
+import cors from 'cors';
+import errorHandler from 'middleware-http-errors';
+import YAML from 'yaml';
+import sui from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import { createClient } from '@vercel/kv';
+
+// Set up web app
+const app = express();
+// Use middleware that allows us to access the JSON body of requests
+app.use(json());
+// Use middleware that allows for access from other domains
+app.use(cors());
+// for logging errors (print to terminal)
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+const KV_REST_API_URL="https://good-kingfish-38504.kv.vercel-storage.com"
+const KV_REST_API_TOKEN="AZZoASQgYTY1ODA0YjktZDA5ZS00MmY5LWJlMGMtOTg0YWQ2NWI1NWQ1MDkyM2IzMzAxOWQ1NDVjOTgwNjhhMWNlNjQ1NzhiMTE="
+const database = createClient({
+  url: KV_REST_API_URL,
+  token: KV_REST_API_TOKEN,
+});
+
+// for producing the docs that define the API
+const file = fs.readFileSync(path.join(process.cwd(), 'swagger.yaml'), 'utf8');
+app.get('/', (req: Request, res: Response) => res.redirect('/docs'));
+app.use(
+  '/docs',
+  sui.serve,
+  sui.setup(YAML.parse(file), {
+    swaggerOptions: { docExpansion: config.expandDocs ? 'full' : 'list' },
+  })
+);
+const PORT: number = parseInt(process.env.PORT || config.port);
+const HOST: string = process.env.IP || 'localhost';
+
+// ====================================================================
+//  ================= WORK IS DONE ABOVE THIS LINE ===================
+// ====================================================================
+
+app.use((req: Request, res: Response) => {
+  const error = `
+    404 Not found - This could be because:
+      0. You have defined routes below (not above) this middleware in server.ts
+      1. You have not implemented the route ${req.method} ${req.path}
+      2. There is a typo in either your test or server, e.g. /posts/list in one
+         and, incorrectly, /post/list in the other
+      3. You are using ts-node (instead of ts-node-dev) to start your server and
+         have forgotten to manually restart to load the new changes
+      4. You've forgotten a leading slash (/), e.g. you have posts/list instead
+         of /posts/list in your server.ts or test file
+  `;
+  res.status(404).json({ error });
+});
+// For handling errors
+app.use(errorHandler());
+
+// start server
+const server = app.listen(PORT, HOST, () => {
+  // DO NOT CHANGE THIS LINE
+  console.log(`⚡️ Server started on port ${PORT} at ${HOST}`);
+});
+
+// For coverage, handle Ctrl+C gracefully
+process.on('SIGINT', () => {
+  server.close(() => console.log('Shutting down server gracefully.'));
+});
