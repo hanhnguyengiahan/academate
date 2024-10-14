58ef94339a2a082fe516f26ebe211d996b28d314
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-10-14 16:07:06 +1100

    impl update and read card

--- back-end/controllers/matching_card.controller.ts
+++ back-end/controllers/matching_card.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import MatchingCard from '../models/matching_card.model';
-import AuthHelper from './helpers/auth.helper';
 import Session from '../models/session.model';
 
 const createCard = async (req: Request, res: Response) => {
@@ -17,16 +16,59 @@ const createCard = async (req: Request, res: Response) => {
     });
 
     res.status(200).json({
-      ...cardInfo,
-      match_card_id: newCard._id,
+      newCard,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-// const readCard = (req: Request, res: Response) => {};
-// const updateCard = (req: Request, res: Response) => {};
+const readCard = async (req: Request, res: Response) => {
+  try {
+    const { token, match_card_id } = req.body;
+
+    const validSession = await Session.findOne({ token });
+    if (!validSession) {
+      return res.status(401).json({ message: 'Invalid credentatials' });
+    }
+
+    // use lean() option so it returns a Js Object only, not a Mongoose Document (which contains extra stuff we dont care)
+    const { _id, ...other } = await MatchingCard.findOne({
+      match_card_id,
+    }).lean();
+
+    res.status(200).json({
+      match_card_id: _id,
+      ...other,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const updateCard = async (req: Request, res: Response) => {
+  try {
+    const { token, match_card_id, ...newCardInfo } = req.body;
+
+    const validSession = await Session.findOne({ token });
+    if (!validSession) {
+      return res.status(401).json({ message: 'Invalid credentatials' });
+    }
+
+    // use lean() option so it returns a Js Object only, not a Mongoose Document (which contains extra stuff we dont care)
+    await MatchingCard.findByIdAndUpdate(match_card_id, {
+      ...newCardInfo,
+    }).lean();
+
+    const { _id, ...other } = await MatchingCard.findById(match_card_id).lean();
+
+    res.status(200).json({
+      match_card_id: _id,
+      ...other,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 // const deleteCard = (req: Request, res: Response) => {};
 
-export { createCard };
+export { createCard, readCard, updateCard };
--- back-end/routes/matching_card.route.ts
+++ back-end/routes/matching_card.route.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-import { createCard } from '../controllers/matching_card.controller';
+import {
+  createCard,
+  readCard,
+  updateCard,
+} from '../controllers/matching_card.controller';
 
 const router = express.Router();
 
 router.post('/', createCard);
-// router.get('/', login);
-// router.put('/', logout);
+router.get('/', readCard);
+router.put('/', updateCard);
 // router.delete('/', logout);
 
 export default router;
