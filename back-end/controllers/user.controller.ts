cc55d4633feddc4c9f6252f3dceffbf47fe6bf04
hanhnguyengiahan <hanhhan2705@gmail.com>
2024-10-18 03:20:05 +1100

    finish friend route

--- back-end/controllers/user.controller.ts
+++ back-end/controllers/user.controller.ts
@@ -8,15 +8,31 @@ import dotenv from 'dotenv';
 import FriendRequest from '../models/friend_request.model';
 import MatchingCard from '../models/matching_card.model';
 dotenv.config();
-
+const createFriendRequest = async (
+  sender_id: string,
+  receiver_id: Object,
+  matching_card_id: string
+) => {
+  try {
+    // Create and return the new friend request
+    const friendRequest = await FriendRequest.create({
+      sender: sender_id,
+      receiver: receiver_id,
+      matchingCard: matching_card_id,
+    });
+    return friendRequest;
+  } catch (error) {
+    throw new Error('Error creating friend request: ' + error.message);
+  }
+};
 const SECRET_KEY = process.env.JWT_SECRET;
 const send_request = async (req: Request, res: Response) => {
   try {
-    const { token, receiver_id, matching_card_id } = req.body;
-    if (!token || !receiver_id || !matching_card_id) {
+    const { token, matching_card_id } = req.body;
+    if (!token || !matching_card_id) {
       return res
         .status(400)
-        .json({ message: 'Token and receiver ID are required' });
+        .json({ message: 'Token and matchingCard ID are required' });
     }
     const validSession = await Session.findOne({ token });
     if (!validSession) {
@@ -26,13 +42,17 @@ const send_request = async (req: Request, res: Response) => {
     if (!payload.id) {
       return res.status(400).json({ message: 'Invalid token payload' });
     }
+    const matchingCard = await MatchingCard.findById(matching_card_id);
+    if (!matchingCard) {
+      return res.status(400).json({ message: 'Invalid matching card id' });
+    }
     const sender_id = payload.id;
-    const friendRequest = await FriendRequest.create({
-      sender: sender_id,
-      receiver: receiver_id,
-      matchingCard: matching_card_id,
-    });
-
+    const receiver_id = matchingCard.userId;
+    const friendRequest = await createFriendRequest(
+      sender_id,
+      receiver_id,
+      matching_card_id
+    );
     const sender = await User.findById(sender_id);
     if (!sender) {
       return res.status(404).json({ message: 'Sender not found' });
@@ -48,7 +68,7 @@ const send_request = async (req: Request, res: Response) => {
     await receiver.save();
     await sender.save();
 
-    res.status(200).json({ friendRequestId: friendRequest._id});
+    res.status(200).json({ friendRequestId: friendRequest._id });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -84,16 +104,27 @@ const accept_friend_request = async (req: Request, res: Response) => {
     let sender = await User.findById(validRequest.sender);
     sender.friends.push({
       user: current_user,
-      matchingCard: validRequest,
+      matchingCard: validRequest.matchingCard,
     });
 
     current_user.friends.push({
       user: sender,
-      matchingCard: validRequest,
+      matchingCard: validRequest.matchingCard,
     });
+
+    // delete sent request from sender
+    let requestIndex = sender.sent_requests.findIndex((req) => req === friend_request_id);
+    sender.sent_requests.splice(requestIndex, 1);
+
+    // delete received request from receiver
+    requestIndex = current_user.received_requests.findIndex((req) => req === friend_request_id);
+    current_user.received_requests.splice(requestIndex, 1);
 
     // accept needs to delete the friend_request_id out of our database
     await FriendRequest.deleteOne({ _id: friend_request_id });
+    await sender.save();
+    await current_user.save();
+    res.status(200).json({ message: 'Successfully accept friend request' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -180,12 +211,19 @@ const list_friend = async (req: Request, res: Response) => {
 
     let listOfSenderAndCards = await Promise.all(
       current_user.friends.map(async (req) => {
-        return await FriendRequest.findById(req).populate(
-          'sender',
-          'matchingCard'
-        );
+        const user = await User.findById(req.user).lean().select('_id name email gender');
+        const course_code = await MatchingCard.findById(
+          req.matchingCard
+        ).select('course_code');
+        console.log(req.matchingCard);
+        return {
+          ...user,
+          course_code,
+        };
       })
     );
+
+    console.log(listOfSenderAndCards);
     res.status(200).json({
       friends: listOfSenderAndCards,
     });
@@ -194,35 +232,11 @@ const list_friend = async (req: Request, res: Response) => {
   }
 };
 
-// const list_sent_requests = async (req: Request, res: Response) => {
-//     try {
-//         const { token } = req.body;
-//         const validSession = await Session.findOne({ token });
-//         if (!validSession) {
-//             return res.status(401).json({ message: 'Invalid credentatials' });
-//         }
-//         // find the current user
-//         const payload = verify(token, SECRET_KEY) as JwtPayload;
-//         if (!payload.id) {
-//             return res.status(400).json({ message: 'Invalid token payload' });
-//         }
-//         const current_user_id = payload.id;
-//         let current_user = await User.findById(current_user_id);
-//         if (!current_user) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-
-//         res.status(200).json({sent_requests: current_user.sent_requests});
-//     } catch(error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// }
-
 const list_received_requests = async (req: Request, res: Response) => {
   try {
     const { token } = req.body;
     if (!token) {
-        return res.status(400).json({ message: 'Token is required'});
+      return res.status(400).json({ message: 'Token is required' });
     }
     const validSession = await Session.findOne({ token });
     if (!validSession) {
@@ -240,16 +254,18 @@ const list_received_requests = async (req: Request, res: Response) => {
     }
     let listOfSenderAndCards = await Promise.all(
       current_user.received_requests.map(async (req) => {
-        const populatedRequest = await FriendRequest.findById(req)
-        .populate([{path: 'sender'}, {path: 'matchingCard'}])
-
+        const populatedRequest = await FriendRequest.findById(req).populate([
+          { path: 'sender' },
+          { path: 'matchingCard' },
+        ]);
+        // console.log(populatedRequest)
         return {
           sender: populatedRequest.sender,
           matchingCard: populatedRequest.matchingCard,
         };
       })
     );
-
+    console.log('list: ', listOfSenderAndCards);
     return res.status(200).json({
       received_requests: listOfSenderAndCards,
     });
@@ -257,6 +273,7 @@ const list_received_requests = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 export {
   send_request,
   accept_friend_request,
--- back-end/routes/user.route.ts
+++ back-end/routes/user.route.ts
@@ -4,13 +4,15 @@ import {
   accept_friend_request,
   list_friend,
   list_received_requests,
+  remove_current_friend,
 } from '../controllers/user.controller';
 const router = express.Router();
 
+router.get('/friends', list_friend);
 router.post('/send', send_request);
 router.post('/accept', accept_friend_request);
-router.get('/friends', list_friend);
 router.get('/received', list_received_requests);
+router.delete('/remove', remove_current_friend);
 
 export default router;
 
